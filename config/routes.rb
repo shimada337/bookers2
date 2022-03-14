16897be920b4425b33df951e70ceb2ef5d8b0b04
shimada337 <eisukeqoo@icloud.com>
2022-03-14 06:23:27 +0000

    [add]topリンク

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show, :edit]
   resources :users, only: [:index, :show, :edit]
   root to: "homes#top"
+  get 'homes/about' => "homes#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
