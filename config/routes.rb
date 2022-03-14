05a787f6e235e2afcadf84e4160964ce623574ff
shimada337 <eisukeqoo@icloud.com>
2022-03-14 00:23:55 +0000

    [add]resourcesルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  resources :books, only: [:index, :show, :edit]
+  resources :users, only: [:index, :show, :edit]
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
