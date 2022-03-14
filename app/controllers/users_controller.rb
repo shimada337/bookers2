ac1b9389646eea833dba85ae13c205b067442aa3
shimada337 <eisukeqoo@icloud.com>
2022-03-14 13:31:37 +0000

    [add]sign up,log in機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,15 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,)
   end
 end
