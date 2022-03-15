1f11aee639b7f8d87359aaa7c0259f95b2a23e1d
shimada337 <eisukeqoo@icloud.com>
2022-03-15 03:31:47 +0000

    [add]book/indexレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name,)
+    params.require(:user).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :books, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :edit, :create]
   resources :users, only: [:index, :show, :edit]
   root to: "homes#top"
   get 'homes/about' => "homes#about"
