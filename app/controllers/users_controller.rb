79ec77da0560bcd4f090ce62c2110f408ca251b5
shimada337 <eisukeqoo@icloud.com>
2022-03-16 01:08:45 +0000

    [add]user更新後遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,12 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
 
   private
 
