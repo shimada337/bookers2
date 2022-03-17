b29c94e40ec9620800ab5dd4882a85343e711102
shimada337 <eisukeqoo@icloud.com>
2022-03-17 09:34:48 +0000

    [add]投稿フラッシュメッセージ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,8 +8,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+    redirect_to book_path(@book.id), notice: 'You have created book successfully.'
+    else
+      redirect_to books_path(@book.id)
+    end
   end
 
   def show
@@ -24,8 +27,11 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+    redirect_to book_path(@book.id), notice:'You have updated book successfully.'
+    else
+      render :edit
+    end
   end
 
   def destroy
