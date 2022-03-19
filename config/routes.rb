0026851d490791f77f413198f6ce110f2697ce03
shimada337 <eisukeqoo@icloud.com>
2022-03-19 06:27:10 +0000

    [update]aboutページurl

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show, :edit, :create, :destroy, :update]
   resources :users, only: [:index, :show, :edit, :update]
   root to: "homes#top"
-  get 'homes/about' => "homes#about"
+  get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
